186cab8454f3b11e77c708d766e47b09b1f44d8d
Nont <135516134+thapanon-kunop@users.noreply.github.com>
2026-05-17 02:18:14 +0700

    feat: rebuild entire website using Next.js framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,73 @@
-import Image from "next/image";
+import { Header } from "@/components/Header";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <Header>SKT M.2/10</Header>
+      <main>
+        <section className="grid grid-cols-2 gap-5 px-5 pt-5">
+          <div>
+            <Card>
+              <CardHeader>
+                <CardTitle>การนำทาง</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-2">
+                <Link
+                  href={"/budget"}
+                  className={buttonVariants({ className: "d-block w-full" })}
+                >
+                  กลุ่มบริหารงบประมาณ
+                </Link>
+                <Link
+                  href={"/docs/schedule"}
+                  className={buttonVariants({
+                    variant: "secondary",
+                    className: "d-block w-full",
+                  })}
+                >
+                  ตารางเรียน
+                </Link>
+                <Link
+                  href={"/docs/cleaning-duty"}
+                  className={buttonVariants({
+                    variant: "outline",
+                    className: "d-block w-full",
+                  })}
+                >
+                  ตารางเวรประจำวัน
+                </Link>
+              </CardContent>
+            </Card>
+          </div>
+          <div>
+            <Card>
+              <CardHeader>
+                <CardTitle>ช่องทางการติดตาม</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-2">
+                <Link
+                  href={"https://www.instagram.com/sktm.210"}
+                  className={buttonVariants({ className: "d-block w-full" })}
+                >
+                  Instagram
+                </Link>
+                <Link
+                  href={"https://sk-thonburi.ac.th/"}
+                  className={buttonVariants({
+                    variant: "secondary",
+                    className: "d-block w-full",
+                  })}
+                >
+                  เว็บไซต์โรงเรียน
+                </Link>
+              </CardContent>
+            </Card>
+          </div>
+        </section>
       </main>
-    </div>
+    </>
   );
 }
