069ad0814d03c07c8d6c0c0facbebebeb47457d1
Nont <135516134+thapanon-kunop@users.noreply.github.com>
2026-05-17 22:46:37 +0700

    style: adjust margins on voting69 page for better spacing

--- src/app/budget/voting69/page.tsx
+++ src/app/budget/voting69/page.tsx
@@ -14,7 +14,7 @@ export const barRedius = 4;
 export default function Voting69Page() {
   return (
     <>
-      <header className="text-center mt-5">
+      <header className="text-center my-5">
         <h1 className="text-xl sm:text-2xl">สรุปผลการลงมติภายในห้องเรียน</h1>
         <div className="flex justify-center gap-2 flex-wrap">
           <Badge variant={"default"}>ผู้มีสิทธิ์โหวตทั้งหมด: 37 คน</Badge>
