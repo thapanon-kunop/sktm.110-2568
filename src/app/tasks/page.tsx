b9800da804c4e2df268ea9be4a168254f540fe51
Nont <135516134+thapanon-kunop@users.noreply.github.com>
2026-05-18 18:53:27 +0700

    feat: add new tasks page to the project

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,5 @@
+export default function TaskPage() {
+  return (
+    <iframe src="https://www.canva.com/design/DAHBfldklS4/i573TvS_2CyRLb29MzclHg/view?embed" className="h-dvh grow"></iframe>
+  );
+}
