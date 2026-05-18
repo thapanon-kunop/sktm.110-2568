bbb1e0379d1d270cc7c234cbbf22d0a77cfe8d5f
Nont <135516134+thapanon-kunop@users.noreply.github.com>
2026-05-18 19:41:47 +0700

    fix: resolve redirect error to the external source website

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function TaskPage() {
-  window.location.href = "https://myskttask.my.canva.site/";
-  
+  useEffect(() => {
+    window.location.href = "https://myskttask.my.canva.site/";
+  });
+
   return (
     <></>
     // <iframe src="https://www.canva.com/design/DAHBfldklS4/i573TvS_2CyRLb29MzclHg/view?embed" className="" width="100%"></iframe>
