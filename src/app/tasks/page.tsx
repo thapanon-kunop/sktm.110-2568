74b8cf1d6d72a8461521ebbf0ddb72a160dfeda7
Nont <135516134+thapanon-kunop@users.noreply.github.com>
2026-05-18 19:35:55 +0700

    chore: remove tasks page and configure redirect to source website

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function TaskPage() {
+  window.location.href = "https://myskttask.my.canva.site/";
+  
   return (
-    <iframe src="https://www.canva.com/design/DAHBfldklS4/i573TvS_2CyRLb29MzclHg/view?embed" className="h-dvh grow"></iframe>
+    <></>
+    // <iframe src="https://www.canva.com/design/DAHBfldklS4/i573TvS_2CyRLb29MzclHg/view?embed" className="" width="100%"></iframe>
   );
 }
